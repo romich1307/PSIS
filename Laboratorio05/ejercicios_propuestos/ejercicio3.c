18d95e835a22a984bcd6f1f962627e530eef1f22
romich1307 <rcamargoh@unsa.edu.pe>
2026-05-19 09:13:23 -0500

    crenado archivos

---
+++ Laboratorio05/ejercicios_propuestos/ejercicio3.c
@@ -0,0 +1,6 @@
+//Modificar el programa de facturación de clientes de tal modo que puedan obtenerse los
+siguientes listados.
+• Clientes en estado moroso.
+• Clientes en estado pagado con factura mayor de una determinada cantidad.
+Restricciones: Uso arreglos, funciones, así como las estructuras de control selectivas y repetitivas
+//
---
+++ Laboratorio05/ejercicios_propuestos/ejercicio7.c
@@ -0,0 +1,2 @@
+//Escribir un programa para calcular el número de días que hay entre dos fechas; declarar fecha
+//como una estructura.
